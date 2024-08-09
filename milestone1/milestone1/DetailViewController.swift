7e92b402956efcc9b745e0fe70f2adcf47e4d0be
DanoneYoghurt <83756452+DanoneYoghurt@users.noreply.github.com>
2024-08-09 12:51:09 +0300

    day 23 complete

---
+++ milestone1/milestone1/DetailViewController.swift
@@ -0,0 +1,54 @@
+//
+//  DetailViewController.swift
+//  milestone1
+//
+//  Created by Антон Баскаков on 09.08.2024.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    @IBOutlet var imageView: UIImageView!
+    
+    var selectedImage: String?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(shareTapped))
+        
+        if let imageToLoad = selectedImage {
+            imageView.image = UIImage(named: imageToLoad)
+        }
+    }
+    
+    @objc func shareTapped() {
+        guard let image = imageView.image?.jpegData(compressionQuality: 0.8) else {
+            print("No image found")
+            return
+        }
+        
+        guard let imageName = selectedImage?.replacingOccurrences(of: "@3x.png", with: "").capitalized else {
+            print("Unable to load name")
+            return
+        }
+        
+        let avc = UIActivityViewController(activityItems: [image, imageName], applicationActivities: [])
+        avc.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+        present(avc, animated: true)
+        
+    }
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
+}
---
+++ milestone1/milestone1/ViewController.swift
@@ -0,0 +1,53 @@
+//
+//  ViewController.swift
+//  milestone1
+//
+//  Created by Антон Баскаков on 09.08.2024.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+    
+    var pictures = [String]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        navigationController?.navigationBar.prefersLargeTitles = true
+        title = "Countries"
+        
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fm.contentsOfDirectory(atPath: path)
+        
+        for item in items {
+            if item.hasSuffix("@3x.png"){
+                pictures.append(item)
+            }
+        }
+        
+        
+        print(pictures)
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pictures.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Country", for: indexPath)
+        cell.textLabel?.text = pictures[indexPath.row].replacingOccurrences(of: "@3x.png", with: "").capitalized
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let vc = storyboard?.instantiateViewController(withIdentifier: "Detail") as? DetailViewController {
+            vc.selectedImage = pictures[indexPath.row]
+            navigationController?.pushViewController(vc, animated: true)
+        }
+    }
+
+}
+
