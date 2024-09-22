b618271e3e4b70fbf536df92d2cf54e435438023
DanoneYoghurt <83756452+DanoneYoghurt@users.noreply.github.com>
2024-09-22 13:52:06 +0300

    day 64 complete

---
+++ project18/project18/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  project18
+//
+//  Created by Антон Баскаков on 22.09.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        for i in 1 ... 100 {
+            print("Got number \(i)")
+        }
+    }
+
+
+}
+
