be2fd640f11fb4d32ef46df4d3757e2637e95dcc
DanoneYoghurt <83756452+DanoneYoghurt@users.noreply.github.com>
2024-10-08 13:27:34 +0300

    day 81 complete

--- project24.playground/Contents.swift
+++ project24.playground/Contents.swift
@@ -15,3 +15,44 @@ attributedString.addAttribute(.font, value: UIFont.systemFont(ofSize: 16), range
 attributedString.addAttribute(.font, value: UIFont.systemFont(ofSize: 24), range: NSRange(location: 8, length: 1))
 attributedString.addAttribute(.font, value: UIFont.systemFont(ofSize: 32), range: NSRange(location: 10, length: 4))
 attributedString.addAttribute(.font, value: UIFont.systemFont(ofSize: 40), range: NSRange(location: 15, length: 6))
+
+// 1.  Create a String extension that adds a withPrefix() method. If the string already contains the prefix it should return itself; if it doesn’t contain the prefix, it should return itself with the prefix added. For example: "pet".withPrefix("car") should return “carpet”.
+
+extension String {
+    func withPrefix(_ prefix: String) -> String {
+        if self.contains(prefix) {
+            return self
+        } else {
+            return prefix + self
+        }
+    }
+}
+
+let challenge1 = "pet"
+
+print(challenge1.withPrefix("pet"))
+
+print(challenge1.withPrefix("car"))
+
+// 2.  Create a String extension that adds an isNumeric property that returns true if the string holds any sort of number. Tip: creating a Double from a String is a failable initializer.
+
+extension String {
+    func isNumric() -> Bool {
+        return Double(self) != nil
+    }
+}
+
+print("123".isNumric())
+
+print("abc".isNumric())
+
+// 3. Create a String extension that adds a lines property that returns an array of all the lines in a string. So, “this\nis\na\ntest” should return an array with four elements.
+
+extension String {
+    func lines() -> [String] {
+        self.components(separatedBy: .newlines)
+    }
+}
+
+print("this\nis\na\ntest".lines())
+print("".lines())
